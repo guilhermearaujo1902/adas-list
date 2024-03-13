c967b545ddfc1b41eb30f51d9239aa7dc6ddeaf9
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2024-03-12 22:06:33 -0300

    Feature: Menu interativo para a classe MainProduto

--- projetoList/src/br/com/projetoList/MainProduto.java
+++ projetoList/src/br/com/projetoList/MainProduto.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class MainProduto {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		int opcao;
-		
+
 		do {
 			System.out.println("*** Menu de opções ***");
 			System.out.println("[1] - Listar produtos ");
@@ -18,11 +18,29 @@ public class MainProduto {
 			System.out.println("[4] - Excluir         ");
 			System.out.println("[0] - Sair            ");
 			System.out.println("Escolha uma opção: ");
-			
+
 			opcao = scan.nextInt();
-			
-		} while(opcao != 0);
-		
+
+			switch (opcao) {
+			case 1:
+				// Listar os produtos
+				break;
+			case 2:
+				// Adicionar novos elementos
+				break;
+			case 3:
+				// Editar elementos
+				break;
+			case 4:
+				// Excluir elementos
+				break;
+			case 0:
+				System.out.println("Saindo do menu ...");
+				break;
+			}
+
+		} while (opcao != 0);
+
 		scan.close();
 	}
 
